977e657fabf0a299859a6bbda1d889f2fe0d705d
mahlatse <ven.Mahlatse.mabotja@comair.co.za>
2021-10-31 17:51:40 +0200

    Connect DIscount.Grpc to BasketApi

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Basket.API.Entities;
 using System.Net;
+using Basket.API.GrpServices;
+using System.Net.Http;
 
 namespace Basket.API.Controllers
 {
@@ -11,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private IBasketRepository _repository;
+        private readonly GrpServices.DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository ?? throw new System.ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new System.ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("{username}", Name = "GetBasket")]
@@ -29,6 +33,17 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // TODO : Communicate with DIscount.GRPC and
+            // calculate latest prices of products into the shopping cart
+            //consume Discount grpc
+            HttpClient.DefaultProxy = new WebProxy();//quick fix, company proxy preventing http 2
+
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDsiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
---
+++ src/Services/Basket/Basket.API/GrpServices/DiscountGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient ?? throw new System.ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+
+        public async System.Threading.Tasks.Task<CouponModel> GetDsiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
+        }
+    }
+}
